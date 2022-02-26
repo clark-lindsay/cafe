ea1b9aac3922e0e0903af1b3f3a3c73c1ec3692d
clark-lindsay <clarklindsay96@gmail.com>
2022-02-26 17:08:10 -0500

    Revert "Add query module for `Teams` context"
    This reverts commit c7032d0d161334e5022597e2e9cd6d1720ea9a69.
    
    Reverting because I want to re-do my many-to-many association set-up

--- lib/cafe/teams.ex
+++ lib/cafe/teams.ex
@@ -6,8 +6,7 @@ defmodule Cafe.Teams do
   import Ecto.Query, warn: false
   alias Cafe.Repo
 
-  alias Cafe.Teams.{Team, TeamsQuery}
-  alias Cafe.Accounts.User
+  alias Cafe.Teams.Team
 
   @doc """
   Returns the list of teams.
@@ -101,11 +100,5 @@ defmodule Cafe.Teams do
   """
   def change_team(%Team{} = team, attrs \\ %{}) do
     Team.changeset(team, attrs)
-  end
-
-  def list_teams_for_user(%User{} = user) do
-    TeamsQuery.new()
-    |> TeamsQuery.for_user(user)
-    |> Repo.all()
   end
 end

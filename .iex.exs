4b6b406f350ffac581fcc7f86fe2a164e1b697f1
clark-lindsay <clarklindsay96@gmail.com>
2022-02-27 17:42:30 -0500

    De-Conditional-ize fixture import for `iex`
    For the foreseeable future, I want these in here, so let's lean in

--- .iex.exs
+++ .iex.exs
@@ -3,4 +3,4 @@ alias Cafe.Teams.{Team, TeamsQuery}
 alias Cafe.Groups.Group
 alias Cafe.Repo
 
-Mix.env() == "test" && import Cafe.{AccountsFixtures, GroupsFixtures, TeamsFixtures}
+import Cafe.{AccountsFixtures, GroupsFixtures, TeamsFixtures}

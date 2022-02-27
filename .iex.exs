bd90fda92b525eb69bdd79d6763fe25b0c0418ef
clark-lindsay <clarklindsay96@gmail.com>
2022-02-26 22:43:56 -0500

    Add imports for fixtures in `iex`
    Without setting the `MIX_ENV` to "test", the fixture modules are not
    compiled, and executing the start-up `.iex.exs` fails.
    
    I thought that I could fix this by using a short-circuit conditional,
    but I was wrong. Even though it's not doing anything right now, I am
    going to leave it in since it has the same functionality as before and
    will remind me of my effort/ previous avenue of investigation.

--- .iex.exs
+++ .iex.exs
@@ -2,3 +2,5 @@ alias Cafe.Accounts.User
 alias Cafe.Teams.{Team, TeamsQuery}
 alias Cafe.Groups.Group
 alias Cafe.Repo
+
+Mix.env() == "test" && import Cafe.{AccountsFixtures, GroupsFixtures, TeamsFixtures}

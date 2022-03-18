ced3334a3d3f1268d141c9070d4d7e2cd8e024f2
clark-lindsay <clarklindsay96@gmail.com>
2022-03-17 20:58:52 -0400

    Implement PubSub for `GroupTeam` join
    Following the same pattern that was used for the other two modules in
    the `Joins` context

--- lib/cafe/joins/group_teams.ex
+++ lib/cafe/joins/group_teams.ex
@@ -4,7 +4,23 @@ import Ecto.Query, only: [from: 2]
   alias Cafe.Repo
   alias Cafe.Joins.GroupTeams.GroupTeam
 
+  @topic "#{IO.inspect(__MODULE__)}"
+
   def create_group_team(group_id, team_id) do
+    with {:ok, inserted_group_team} <- insert_group_team(group_id, team_id),
+         :ok <- publish_create_group_team(inserted_group_team),
+    do: {:ok, inserted_group_team}
+  end
+
+  def remove_group_team(group_id, team_id) do
+    group_team = %GroupTeam{group_id: group_id, team_id: team_id}
+
+    with :ok <- delete_group_team(group_team),
+         :ok <- publish_remove_group_team(group_team),
+    do: :ok
+  end
+
+  defp insert_group_team(group_id, team_id) do
     query =
       from(g in GroupTeam,
         where: g.group_id == ^group_id and g.team_id == ^team_id
@@ -21,7 +37,7 @@ import Ecto.Query, only: [from: 2]
     end
   end
 
-  def remove_group_team(group_id, team_id) do
+  defp delete_group_team(%GroupTeam{group_id: group_id, team_id: team_id}) do
     to_be_removed =
       from(g in GroupTeam,
         where: g.group_id == ^group_id and g.team_id == ^team_id
@@ -29,4 +45,14 @@ import Ecto.Query, only: [from: 2]
 
     Repo.delete_all(to_be_removed)
   end
+
+  defp publish_create_group_team(%GroupTeam{} = group_team) do
+    Phoenix.PubSub.broadcast(Cafe.PubSub, @topic, {:create_group_team, group_team})
+  end
+
+  defp publish_remove_group_team(%GroupTeam{} = group_team) do
+    Phoenix.PubSub.broadcast(Cafe.PubSub, @topic, {:remove_group_team, group_team})
+  end
+
+  def pubsub_topic(), do: @topic
 end
